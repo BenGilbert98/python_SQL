55eb6cbbaa3826216680f732167b8f447acc05b8
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-12 15:23:44 +0000

    added add_movie and remove_movie functions

--- SQL_movies_crud.py
+++ SQL_movies_crud.py
@@ -6,6 +6,7 @@ df = pd.DataFrame(data, columns=['titleType', 'primaryTitle', 'originalTitle', '
                                  'runtimeMinutes', 'genres'])
 # print(df)
 
+
 server = 'databases1.spartaglobal.academy'
 database = 'Northwind'
 username = '********'
@@ -41,3 +42,29 @@ print(sql_query)
 exported_data = pd.read_sql_query('''SELECT * FROM imdb_movies''', conn)
 df_2 = pd.DataFrame(exported_data)
 df.to_csv(r'C:\Users\bengi\PycharmProjects\python_SQL\imdbtitlesfromsql.csv')
+
+
+def add_movie():
+    titleType = input("What type of movie is it?    ")
+    primaryTitle = input("What is the primary title?    ")
+    originalTitle = input("What is the original title?    ")
+    isAdult = input("is the move rated 18+?  (1 for yes, 0 for no)   ")
+    startYear = input("What is the start year?    ")
+    endYear = input("What is the end year?    ")
+    runtimeMinutes = input("What is the movies runtime (minutes)?    ")
+    genres = input("What genre is the movie?    ")
+
+    cursor.execute(f"""
+       INSERT INTO imdb_movies (titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres)
+       VALUES ({titleType}, {primaryTitle}, {originalTitle}, {isAdult}, {startYear}, {endYear}, {runtimeMinutes}, {genres})
+       """)
+
+    conn.commit()
+
+
+def remove_movie():
+    movie = input("What is the primary title of the movie you want to remove?   ")
+    cursor.execute(f"""
+           DELETE FROM imdb_movies WHERE primaryTitle = {movie}
+           """)
+    conn.commit()

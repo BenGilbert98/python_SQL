9f19d9bb4b0a97989a55bf2339e7f19245493180
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-12 16:10:53 +0000

    updated SQL movies_crud with functions for querying and for writing csv files

--- SQL_movies_crud.py
+++ SQL_movies_crud.py
@@ -9,62 +9,56 @@ df = pd.DataFrame(data, columns=['titleType', 'primaryTitle', 'originalTitle', '
 
 server = 'databases1.spartaglobal.academy'
 database = 'Northwind'
-username = '********'
-password = '***'
+username = 'SA'
+password = 'Passw0rd2018'
 driver = '{ODBC Driver 17 for SQL Server}'
 
 conn = pyodbc.connect(
     'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 cursor = conn.cursor()
 
-cursor.execute(
-    'CREATE TABLE imdb_movies (titleType VARCHAR(255), primaryTitle VARCHAR(255), originalTitle VARCHAR(255), isAdult INT, startYear INT, endYear nvarchar(255), runtimeMinutes nvarchar(255), genres nvarchar(255))')
 
-for row in df.itertuples():
-    cursor.execute("""
-    INSERT INTO imdb_movies (titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    """,
-                   row.titleType,
-                   row.primaryTitle,
-                   row.originalTitle,
-                   row.isAdult,
-                   row.startYear,
-                   row.endYear,
-                   row.runtimeMinutes,
-                   row.genres)
-
-    conn.commit()
-
-sql_query = pd.read_sql_query('SELECT * FROM imdb_movies', conn)
-print(sql_query)
-
-exported_data = pd.read_sql_query('''SELECT * FROM imdb_movies''', conn)
-df_2 = pd.DataFrame(exported_data)
-df.to_csv(r'C:\Users\bengi\PycharmProjects\python_SQL\imdbtitlesfromsql.csv')
+def delete():
+    cursor.execute(
+        'DELETE FROM imdb_movies_ben')
 
 
-def add_movie():
-    titleType = input("What type of movie is it?    ")
-    primaryTitle = input("What is the primary title?    ")
-    originalTitle = input("What is the original title?    ")
-    isAdult = input("is the move rated 18+?  (1 for yes, 0 for no)   ")
-    startYear = input("What is the start year?    ")
-    endYear = input("What is the end year?    ")
-    runtimeMinutes = input("What is the movies runtime (minutes)?    ")
-    genres = input("What genre is the movie?    ")
-
-    cursor.execute(f"""
-       INSERT INTO imdb_movies (titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres)
-       VALUES ({titleType}, {primaryTitle}, {originalTitle}, {isAdult}, {startYear}, {endYear}, {runtimeMinutes}, {genres})
-       """)
-
-    conn.commit()
+def create():
+    cursor.execute(
+        'CREATE TABLE imdb_movies_ben (titleType VARCHAR(255), primaryTitle VARCHAR(255), originalTitle VARCHAR(255), isAdult INT, startYear INT, endYear nvarchar(255), runtimeMinutes nvarchar(255), genres nvarchar(255))')
 
 
-def remove_movie():
-    movie = input("What is the primary title of the movie you want to remove?   ")
-    cursor.execute(f"""
-           DELETE FROM imdb_movies WHERE primaryTitle = {movie}
-           """)
-    conn.commit()
+def insert_csv_data():
+    for row in df.itertuples():
+        cursor.execute("""
+            INSERT INTO imdb_movies_ben (titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+                       row.titleType,
+                       row.primaryTitle,
+                       row.originalTitle,
+                       row.isAdult,
+                       row.startYear,
+                       row.endYear,
+                       row.runtimeMinutes,
+                       row.genres)
+
+        conn.commit()
+
+
+def sql_query():
+    query = input("Please enter your sql query    ")
+    exported_data = pd.read_sql_query(f'{query}', conn)
+    df_2 = pd.DataFrame(exported_data)
+    print(df_2)
+
+
+def sql_to_csv():
+    query = input("Please enter your sql query    ")
+    exported_data = pd.read_sql_query(f'{query}', conn)
+    df_2 = pd.DataFrame(exported_data)
+    print(df_2)
+    df_2.to_csv(r'C:\Users\bengi\PycharmProjects\python_SQL\imdbtitlesfromsqlten.csv')
+
+
+sql_query()
